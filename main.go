28725cdb85418e2d8272facf1fbafe1a357bcfbb
Francesco Palma <117196189+FrancescoPalmaDev@users.noreply.github.com>
2026-07-06 19:30:36 +0200

    bet function

--- main.go
+++ main.go
@@ -1,5 +1,38 @@
 package main
 
-func main() {
+import "fmt"
 
+func main() {
+	balance := uint(200)
+	getName()
+	bet := getBet(balance)
+	fmt.Println(bet)
+}
+
+func getName() string {
+	name := ""
+
+	fmt.Println("Welcome to Slotmachine App")
+	fmt.Printf("Enter your name: ")
+	_, err := fmt.Scanln(&name)
+	if err != nil {
+		return ""
+	}
+	fmt.Printf("Welcome %s", name)
+	return name
+}
+
+func getBet(balance uint) uint {
+	var bet uint
+	for true {
+		fmt.Printf("Enter your bet (balance = $%d): ", balance)
+		fmt.Scan(&bet)
+
+		if bet > balance {
+			fmt.Println("Bet cannot be larger than the balance")
+		} else {
+			break
+		}
+	}
+	return bet
 }
