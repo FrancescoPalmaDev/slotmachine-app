69a217312a6be560f5ceda340be97cf361de705f
Francesco Palma <117196189+FrancescoPalmaDev@users.noreply.github.com>
2026-07-06 20:54:01 +0200

    spin logic

--- main.go
+++ main.go
@@ -1,12 +1,72 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"math/rand"
+)
 
 func main() {
+	symbols := map[string]uint{
+		"A": 4,
+		"B": 7,
+		"C": 12,
+		"D": 20,
+	}
+	/*multipliers := map[string]uint{
+		"A": 20,
+		"B": 10,
+		"C": 5,
+		"D": 2,
+	}*/
+	symbolArr := generateSymbolArray(symbols)
+	spin := getSpin(symbolArr, 3, 3)
+	fmt.Println(spin)
+
 	balance := uint(200)
 	getName()
 	bet := getBet(balance)
 	fmt.Println(bet)
+	balance = balance - bet
+	fmt.Println("Your current balance is: ", balance)
+}
+
+func getRandomNumber(min int, max int) int {
+	randomNumber := rand.Intn(max-min+1) + min
+	return randomNumber
+}
+
+func getSpin(reel []string, rows int, cols int) [][]string {
+	result := [][]string{}
+
+	for i := 0; i < rows; i++ {
+		result = append(result, []string{})
+	}
+
+	for col := 0; col < cols; col++ {
+		selected := map[int]bool{}
+		for row := 0; row < rows; row++ {
+			for true {
+				randomIndex := getRandomNumber(0, len(reel)-1)
+				_, exists := selected[randomIndex]
+				if !exists {
+					selected[randomIndex] = true
+					result[row] = append(result[row], reel[randomIndex])
+					break
+				}
+			}
+		}
+	}
+	return result
+}
+
+func generateSymbolArray(symbols map[string]uint) []string {
+	symbolSlice := []string{}
+	for symbols, count := range symbols {
+		for i := uint(0); i < count; i++ {
+			symbolSlice = append(symbolSlice, symbols)
+		}
+	}
+	return symbolSlice
 }
 
 func getName() string {
@@ -18,7 +78,7 @@ func getName() string {
 	if err != nil {
 		return ""
 	}
-	fmt.Printf("Welcome %s", name)
+	fmt.Printf("Welcome %s ", name)
 	return name
 }
 
