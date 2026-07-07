0279807f18a1e4ff3346afa394f587d08f11a3a1
Francesco Palma <117196189+FrancescoPalmaDev@users.noreply.github.com>
2026-07-07 18:14:32 +0200

    check win logic

--- main.go
+++ main.go
@@ -12,12 +12,13 @@ func main() {
 		"C": 12,
 		"D": 20,
 	}
-	/*multipliers := map[string]uint{
+	multipliers := map[string]uint{
 		"A": 20,
 		"B": 10,
 		"C": 5,
 		"D": 2,
-	}*/
+	}
+
 	symbolArr := generateSymbolArray(symbols)
 
 	balance := uint(200)
@@ -32,8 +33,38 @@ func main() {
 		balance -= bet
 		spin := getSpin(symbolArr, 3, 3)
 		printSpin(spin)
+		winningLines := checkWin(spin, multipliers)
+
+		for i, multi := range winningLines {
+			win := multi * bet
+			balance += win
+			if multi > 0 {
+				fmt.Printf("Won $%d, (%dx) on Line #%d\n", win, multi, i+1)
+			}
+		}
 	}
 	fmt.Println("Your current balance is: ", balance)
+}
+
+func checkWin(spin [][]string, multipliers map[string]uint) []uint {
+	lines := []uint{}
+
+	for _, row := range spin {
+		win := true
+		checkSymbol := row[0]
+		for _, symbol := range row[1:] {
+			if checkSymbol != symbol {
+				win = false
+				break
+			}
+		}
+		if win {
+			lines = append(lines, multipliers[checkSymbol])
+		} else {
+			lines = append(lines, 0)
+		}
+	}
+	return lines
 }
 
 func printSpin(spin [][]string) {
